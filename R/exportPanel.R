85659d6433a30b8322f5e335aa3e599a48496e98
mutterer <jerome.mutterer@gmail.com>
2019-02-05 13:53:23 +0100

    add ij prefs path for win10

--- R/exportPanel.R
+++ R/exportPanel.R
@@ -6,45 +6,52 @@
 exportPanel <- function() {
   # rstudioapi::callFun("savePlotAsImage","/Users/jmutterer/Desktop/test.png", "png", 640, 480)
   if (grepl("apple", Sys.getenv("R_PLATFORM"))) {
-  f <-
-    paste(Sys.getenv("HOME"),
-          "/Library/Preferences/IJ_Prefs.txt",
-          sep = "")
-} else {
-  f <-
-    paste(Sys.getenv("HOME"),
-          ".imagej",
-          "IJ_Prefs.txt",
-          sep = .Platform$file.sep)
-  
-}
-
+    f <-
+      paste(Sys.getenv("HOME"),
+            "/Library/Preferences/IJ_Prefs.txt",
+            sep = "")
+  } else {
+    f <-
+      paste(Sys.getenv("HOME"),
+            ".imagej",
+            "IJ_Prefs.txt",
+            sep = .Platform$file.sep)
+    
+  }
+  # will make it work on Win10 for now. better method needed
+  if (file.exists (f)) {
+    f <-
+      paste(Sys.getenv("USERPROFILE"),
+            ".imagej",
+            "IJ_Prefs.txt",
+            sep = .Platform$file.sep)
+  }
   print (paste("prefs file: ",f))
-
-if (file.exists (f)) {
-  ijPrefs <-
-    read.table(
-      f,
-      header = FALSE,
-      sep = "=",
-      fill= T,
-      col.names = c("Key", "Value")
-    )
-  print ("done reading prefs file")
-  panelTempDir <-
-    paste("", with(ijPrefs, Value[Key == ".figurej.tempDir"]), sep = "")
-  panelFilename <-
-    paste("", with(ijPrefs, Value[Key == ".figurej.panelFilename"]), sep =
-            "")
-  panelWidth <-
-    paste("", with(ijPrefs, Value[Key == ".figurej.panelWidth"]), sep = "")
-  panelHeight <-
-    paste("", with(ijPrefs, Value[Key == ".figurej.panelHeight"]), sep = "")
-  print (paste("temp dir:",panelTempDir))
-  print (paste("fname:",panelFilename))
-  print (paste("width:",panelWidth))
-  print (paste("height:",panelHeight))
-}
+  
+  if (file.exists (f)) {
+    ijPrefs <-
+      read.table(
+        f,
+        header = FALSE,
+        sep = "=",
+        fill= T,
+        col.names = c("Key", "Value")
+      )
+    print ("done reading prefs file")
+    panelTempDir <-
+      paste("", with(ijPrefs, Value[Key == ".figurej.tempDir"]), sep = "")
+    panelFilename <-
+      paste("", with(ijPrefs, Value[Key == ".figurej.panelFilename"]), sep =
+              "")
+    panelWidth <-
+      paste("", with(ijPrefs, Value[Key == ".figurej.panelWidth"]), sep = "")
+    panelHeight <-
+      paste("", with(ijPrefs, Value[Key == ".figurej.panelHeight"]), sep = "")
+    print (paste("temp dir:",panelTempDir))
+    print (paste("fname:",panelFilename))
+    print (paste("width:",panelWidth))
+    print (paste("height:",panelHeight))
+  }
   ### make sure temp dir exists, or create it
   if (file.exists (panelTempDir)) {
     print ("temp dir does exists")
@@ -53,9 +60,9 @@ if (file.exists (f)) {
     dir.create(panelTempDir)
   }
   
-if ((panelTempDir != "") &&
-    (panelFilename != "") && (panelWidth != "") && (panelHeight != "")) {
-  if (nchar(f) > 0) {
+  if ((panelTempDir != "") &&
+      (panelFilename != "") && (panelWidth != "") && (panelHeight != "")) {
+    if (nchar(f) > 0) {
       context <- rstudioapi::getSourceEditorContext()
       code <- context$contents
       cat("code")
@@ -66,16 +73,16 @@ if ((panelTempDir != "") &&
       cat(code)
       cat("\n # end of code\n")
       sink(NULL)
-    print (paste (panelTempDir, panelFilename, panelWidth, panelHeight))
-    rstudioapi::callFun(
-      "savePlotAsImage",
-      paste(panelTempDir, panelFilename, sep = ""),
-      "tif",
-      as.numeric(panelWidth),
-      as.numeric(panelHeight)
-    )
-  }
-} 
+      print (paste (panelTempDir, panelFilename, panelWidth, panelHeight))
+      rstudioapi::callFun(
+        "savePlotAsImage",
+        paste(panelTempDir, panelFilename, sep = ""),
+        "tif",
+        as.numeric(panelWidth),
+        as.numeric(panelHeight)
+      )
+    }
+  } 
 }
 # explicit call for debugging before updating github
 # exportPanel()
